bb4fc2e25549ddbf07da6bf580cb8581946b6b33
romero-guillermo <guilleliketio@gmail.com>
2023-08-30 13:01:53 -0300

    cambios

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const miDiv = document.getElementById("container");
+
+  miDiv.addEventListener("click", function() {
+    alert("Hola! Soy el div");
+  });
+});
